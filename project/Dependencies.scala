ec94932286bd77a7624d95d6cfb8e36c904f1fe7
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-08-18 19:38:29 +0200

    Update zio-schema, zio-schema-json, ... to 1.8.6 (#470)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -78,7 +78,7 @@ object Dependencies {
 
   object ZIO {
     val zioVersion = "2.1.26"
-    val schemaVersion = "1.7.6"
+    val schemaVersion = "1.8.6"
     val core = "dev.zio" %% "zio" % zioVersion
 
     val http = "dev.zio" %% "zio-http" % "3.0.1"
